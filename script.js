e9cb15246bef166d1ba00b793a0579c0935b6fcd
rishabhgit2 <rishabhkumarrmishra@gmail.com>
2025-05-12 15:18:45 +0530

    added index.html

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  document.getElementById("formMsg").textContent = "Thank you for reaching out!";
+  this.reset();
+});
